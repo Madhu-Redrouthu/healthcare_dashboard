badc276bcf39b451b421093a48c33a969650f486
Madhu Redrouthu <139635936+Madhu-Redrouthu@users.noreply.github.com>
2025-11-04 17:48:50 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,6 +17,6 @@ ALLOWED_HOSTS = [
 ]
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ]
 }
